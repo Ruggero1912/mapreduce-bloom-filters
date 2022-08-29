f0c67c4e1dd7219d5e6729eebe6b804d59f4fb14
Matteo Pierucci <matteo.pierucci4@gmail.com>
2022-08-29 17:37:37 +0200

    Optimizations and fixes in job3

--- src/main/java/it/unipi/moviesBloomFilters/job3/FiltersTestInMapperCombiner.java
+++ src/main/java/it/unipi/moviesBloomFilters/job3/FiltersTestInMapperCombiner.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.StringTokenizer;
 
-public class FiltersTestInMapperCombiner extends Mapper<Object, Text, IntWritable, IntArrayWritable> {
+public class FiltersTestInMapperCombiner extends Mapper<Object, Text, IntWritable, Text> {
 
     private static final String  BLOOM_FILTERS_PATH = "";
     private static final int N_FILTERS = 10;
@@ -23,9 +23,15 @@ public class FiltersTestInMapperCombiner extends Mapper<Object, Text, IntWritabl
     private int[] counterFN = new int[N_FILTERS];
     private int[] counterTP = new int[N_FILTERS];
     private int[] counterTN = new int[N_FILTERS];
+    private final IntWritable key = new IntWritable();
+    private final Text value = new Text();
 
     public void setup(Context ctx) throws IOException {
+
         URI[] cachedFiles = ctx.getCacheFiles();
+        IntWritable key = new IntWritable();
+        BloomFilter filter = new BloomFilter();
+
         for (URI fileStatus : cachedFiles) {
             if (!fileStatus.getPath().toString().endsWith("_SUCCESS")) {
 
@@ -35,14 +41,12 @@ public class FiltersTestInMapperCombiner extends Mapper<Object, Text, IntWritabl
                 boolean hasNext;
 
                 do {
-                    IntWritable key = new IntWritable();
-                    BloomFilter filter = new BloomFilter();
                     hasNext = reader.next(key, filter);
                     if(key == null || filter == null || filter.getK() == 0){
                         continue;
                     }
-                    System.out.println("ho letto chiave: " + key.get() + " | bloom filter: " + filter.toString());
-                    this.bloomFilters[key.get() - 1] = filter;
+                    // System.out.println("ho letto chiave: " + key.get() + " | bloom filter: " + filter.toString());
+                    this.bloomFilters[key.get() - 1] = new BloomFilter(filter);
                 } while (hasNext);
             }
         }
@@ -51,44 +55,52 @@ public class FiltersTestInMapperCombiner extends Mapper<Object, Text, IntWritabl
     public void map(Object key, Text value, Context context){
 
         /* value is a row of the dataset, it represents a movie */
-        StringTokenizer itr = new StringTokenizer(value.toString(), "\t");
+        String record = value.toString();
+        if(record == null || record.length() == 0)
+            return;
 
-        String movieID = itr.nextToken();
-        int roundedRating = Math.round(Float.parseFloat(itr.nextToken()));
+        String[] tags = value.toString().split("\t");
+
+        if(tags.length != 3)
+            return;
+
+        int roundedRating = Math.round(Float.parseFloat(tags[1]));
+        if (roundedRating == 0)
+            return;
 
         int i = 1;
-        System.out.println("=============================");
-        System.out.println("RECORD (id, rating): " + movieID + " " + roundedRating);
         for (BloomFilter bloomFilter:
              bloomFilters) {
 
-            System.out.println("TEST FILTER: " + i + " | CONTENUTO: " + bloomFilter);
-
-            if (bloomFilter != null) {
-                if (bloomFilter.check(movieID)) {
-                    System.out.println("IL FILM E' NEL BLOOM FILTER? " + bloomFilter.check(movieID));
+            if (bloomFilter != null && bloomFilter.isInitialized()){
+                if(bloomFilter.check(tags[0])){
                     if (i == roundedRating)
-                        counterTP[i - 1] += 1;
+                        counterTP[i-1] += 1;
                     else
-                        counterFP[i - 1] += 1;
-                } else {
+                        counterFP[i-1] += 1;
+                } else{
                     if (i != roundedRating)
-                        counterTN[i - 1] += 1;
+                        counterTN[i-1] += 1;
                     else
-                        counterFN[i - 1] += 1;
+                        counterFN[i-1] += 1;
                 }
             }
-                i += 1;
+            i += 1;
         }
-        System.out.println("=============================");
     }
 
     public void cleanup(Context ctx) throws IOException, InterruptedException {
-        System.out.println("CLEANUP");
         for (int i = 0; i < N_FILTERS; i++) {
-            Integer[] stats = {counterFP[i], counterFN[i], counterTP[i], counterTN[i]};
-            System.out.println("STATS ["+ (i + 1) + "]: " + counterFP[i] + " " + counterFN[i] + " " + counterTP[i] + " " + counterTN[i]);
-            ctx.write(new IntWritable(i+1), new IntArrayWritable(stats));
+
+            value.set(  String.valueOf(counterFP[i]) + ',' +
+                        String.valueOf(counterFN[i]) + ',' +
+                        String.valueOf(counterTP[i]) + ',' +
+                        String.valueOf(counterTN[i]));
+
+            //System.out.println("Chiave: " + (i+1) + " stats: " + value);
+
+            key.set(i+1);
+            ctx.write(  key, value);
         }
     }
 }
--- src/main/java/it/unipi/moviesBloomFilters/job3/FiltersTestReducer.java
+++ src/main/java/it/unipi/moviesBloomFilters/job3/FiltersTestReducer.java
@@ -1,37 +1,36 @@
 package it.unipi.moviesBloomFilters.job3;
 
-import it.unipi.moviesBloomFilters.BloomFilter;
 import it.unipi.moviesBloomFilters.IntArrayWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
-public class FiltersTestReducer extends Reducer<IntWritable, IntArrayWritable, IntWritable, IntArrayWritable> {
+public class FiltersTestReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
 
-    private static final int N_FILTERS = 10;
-
-    public void reduce(IntWritable rating, Iterable<IntArrayWritable> counts, Context ctx) throws IOException, InterruptedException {
+    public void reduce(IntWritable rating, Iterable<Text> counters, Context ctx) throws IOException, InterruptedException {
         int counterFP = 0;
         int counterFN = 0;
         int counterTP = 0;
         int counterTN = 0;
 
-        System.out.println("REDUCER FOR RATING " + rating);
+        for (Text stats:
+             counters) {
 
-        for (IntArrayWritable count: counts) {
-            Integer[] countsArray = count.toArray();
-            for (int i = 0; i < countsArray.length; i++) {
-                counterFP += countsArray[0];
-                counterFN += countsArray[1];
-                counterTP += countsArray[2];
-                counterTN += countsArray[3];
-            }
+            StringTokenizer itr = new StringTokenizer(stats.toString(), ",");
+
+            counterFP += Integer.parseInt(itr.nextToken());
+            counterFN += Integer.parseInt(itr.nextToken());
+            counterTP += Integer.parseInt(itr.nextToken());
+            counterTN += Integer.parseInt(itr.nextToken());
         }
 
-        System.out.println("FP: " + counterFP + " | FN: " + counterFN + " | TP: " + counterTP + " | TN: " + counterTN);
-
-        Integer[] finalCounts = {counterFP, counterFN, counterTP, counterTN};
-        ctx.write(rating, new IntArrayWritable(finalCounts));
+        Text finalCounts =    new Text( String.valueOf(counterFP) + ',' +
+                                        String.valueOf(counterFN) + ',' +
+                                        String.valueOf(counterTP) + ',' +
+                                        String.valueOf(counterTN));
+        ctx.write(rating, finalCounts);
     }
 }
