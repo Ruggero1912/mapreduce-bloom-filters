9b3c1e49aaa83e46219628f1bb2b765f5e21ab36
Matteo Pierucci <matteo.pierucci4@gmail.com>
2022-08-29 17:35:40 +0200

    Changes to the IntArrayMapper class

--- src/main/java/it/unipi/moviesBloomFilters/IntArrayWritable.java
+++ src/main/java/it/unipi/moviesBloomFilters/IntArrayWritable.java
@@ -5,26 +5,18 @@ import org.apache.hadoop.io.IntWritable;
 
 public class IntArrayWritable extends ArrayWritable {
 
-    private IntWritable[] writableValues;
-
-    public IntArrayWritable() {
-        super(IntWritable.class);
+    public IntArrayWritable(IntWritable[] values) {
+        super(IntWritable.class, values);
     }
 
-    public IntArrayWritable(Integer[] values) {
-        super(IntWritable.class);
-        writableValues = new IntWritable[values.length];
-        for (int i = 0; i < values.length; i++) {
-            writableValues[i] = new IntWritable(values[i]);
-        }
-        set(writableValues);
+    @Override
+    public IntWritable[] get() {
+        return (IntWritable[]) super.get();
     }
 
-    public Integer[] toArray(){
-        Integer[] arrayValues = new Integer[this.writableValues.length];
-        for (int i = 0; i < this.writableValues.length; i++) {
-            arrayValues[i] = this.writableValues[i].get();
-        }
-        return arrayValues;
+    @Override
+    public String toString() {
+        IntWritable[] values = get();
+        return values[0].toString() + ", " + values[1].toString();
     }
 }
